f9ea8da2af4ccce5ccb2cb1c20d316d950c7d5f3
kamuju25 <naveenknk0308@gmail.com>
2026-04-15 12:24:00 -0400

    k8s

---
+++ terraform/outputs.tf
@@ -0,0 +1,25 @@
+output "cluster_name" {
+    description = "EKS cluster name"
+    value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+    description = "EKS cluster API endpoint"
+    value = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority" {
+    description = "EKS cluster CA certificate (base64)"
+    value = module.eks.cluster_certificate_authority_data
+    sensitive = true
+}
+
+output "vpc_id" {
+    description = "VPC ID"
+    value = module.vpc.vpc_id
+}
+
+output "region" {
+    description = "AWS region"
+    value = var.aws_region
+}
